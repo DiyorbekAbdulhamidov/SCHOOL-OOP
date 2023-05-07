8ed87a80f716642a47a9c7e6d2721e83f92189fa
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-05-07 22:03:15 +0500

    codes

--- index.ts
+++ index.ts
@@ -3,15 +3,15 @@ import { Main } from "./src/main";
 let main = new Main();
 main.addStudenttoGroup();
 main.addGroupToSchool();
-// console.log(main.getStudentBySchoolNum(12));
-// console.log(main.getTeacherBySchoolNum(5));
-// console.log(main.getStudentByGroup("9-V"));
-// console.log(main.getTeacherByPredmet("MATEMATIKA"));
-// console.log(main.getSchoolByNum(12));
-// console.log(main.getPredmetByTeacher("Lola"));
-// console.log(main.getGroupBySchoolNum(12));
-// console.log(main.getAllStudents());
-// console.log(main.getAllGroups());
-// console.log(main.getAllSchools());
+console.log(main.getStudentBySchoolNum(12));
+console.log(main.getTeacherBySchoolNum(5));
+console.log(main.getStudentByGroup("9-V"));
+console.log(main.getTeacherByPredmet("MATEMATIKA"));
+console.log(main.getSchoolByNum(12));
+console.log(main.getPredmetByTeacher("Lola"));
+console.log(main.getGroupBySchoolNum(12));
+console.log(main.getAllStudents());
+console.log(main.getAllGroups());
+console.log(main.getAllSchools());
 
 // async / await
